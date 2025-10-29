88097546a632a46121e7d5bf8df6219cb06a6583
Voixerepo <marloa18@icloud.com>
2025-10-28 17:01:22 -0700

    _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,12 @@
+// pages/_app.tsx
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import '../styles/globals.css'
-import TopBar from '../components/TopBar'
-import { CartProvider } from '../components/CartProvider'
-import CartDrawer from '../components/CartDrawer'
+import '../styles/globals.css'                 // ← important
+import { CartProvider } from '../components/CartProvider' // if you’re using the cart
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta name="theme-color" content="#0a0a0a" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-
-      <CartProvider>
-        <TopBar />
-        {/* Drawer lives at root so it works from any page */}
-        <CartDrawer />
-        <Component {...pageProps} />
-      </CartProvider>
-    </>
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
   )
 }
