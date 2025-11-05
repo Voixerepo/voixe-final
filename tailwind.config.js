bfe138a32341d034ab2293bdddeb2c93ae6fee37
Voixerepo <marloa18@icloud.com>
2025-11-04 20:08:07 -0800

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,8 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  theme: { extend: {} },
+  plugins: []
 }
