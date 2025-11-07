b7cfc636464211f345941c2c3c6b6bad3df5c1b7
Voixerepo <marloa18@icloud.com>
2025-11-07 01:04:09 -0800

    _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,11 @@
-import type { AppProps } from 'next/app'
-import '../styles/globals.css'
-import { CartProvider } from '../components/CartProvider'
-import TopBar from '../components/TopBar'
-import CartDrawer from '../components/CartDrawer'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { CartProvider } from "../components/CartProvider";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <CartProvider>
-      <TopBar />
-      <CartDrawer />
       <Component {...pageProps} />
     </CartProvider>
-  )
+  );
 }
