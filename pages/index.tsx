4eebab2e2cf8d677768ae56a556b4ba44a9ac2ca
Voixerepo <marloa18@icloud.com>
2025-11-07 01:28:13 -0800

    index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,11 @@ import Animate from "../components/Animate";
 export default function Home() {
   return (
     <main className="page-wrap">
-      <Animate as="h1" variant="fade-up" className="" delay={0}>
-        VOIXE
-      </Animate>
-
-      <Animate as="p" variant="fade-up" className="" delay={1}>
+      <Animate as="h1" variant="fade-up">VOIXE</Animate>
+      <Animate as="p" variant="fade-up" delay={1}>
         <em>“To express who you are without saying a word.”</em>
       </Animate>
-
-      <Animate as="div" variant="fade-up" delay={2} className="" >
+      <Animate as="div" variant="fade-up" delay={2}>
         <div style={{ marginTop: 16, display: "flex", gap: 10 }}>
           <a className="btn-supreme-black" href="/shop">shop</a>
           <a className="btn-outline" href="/lookbook">lookbook</a>
